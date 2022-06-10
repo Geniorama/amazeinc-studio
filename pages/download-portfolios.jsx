690fe28c322ee9560055a008f066151c78a7b070
DiegoFreelance <diego.burgos.mesa95@gmail.com>
2022-06-09 22:13:11 -0500

    about y download

--- pages/download-portfolios.jsx
+++ pages/download-portfolios.jsx
@@ -1,16 +1,59 @@
-import Layout from "../components/layout/Layout"
+import Layout from "../components/layout/Layout";
+import styles from "../styles/DownloadPortfolios.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function DownloadPortfolios() {
-  return(
+  return (
     <Layout
       title={"AmazeInc Studio"}
       idPage={"amaze-download"}
       header={"principal"}
       headerFixed={true}
+      footer={true}
     >
-
-
-
+      <div className={styles.contPortfolios}>
+        <div className="container">
+          <div className={styles.contGeneral}>
+            <div className={styles.contTitle}>
+              <h2 className={styles.title}>PORTFOLIOS</h2>
+            </div>
+            <div className={styles.contItems}>
+              <ul>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+                <a href="#">
+                  <li>
+                    <span>CAMPAINGS</span>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )  
+  );
 }
