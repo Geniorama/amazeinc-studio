bb090571ef910aba3ab2518e5b16e9de2a256e66
Geniorama Agencia <geniorama.info@gmail.com>
2022-06-28 21:16:49 -0500

    fix: error boundary component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import { appWithTranslation } from "next-i18next"
+import ErrorBoundary from '../components/ErrorBoundary'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
 
 export default appWithTranslation(MyApp)
